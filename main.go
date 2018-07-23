5bc74b345f777c22b2f4d826e5d5ffb6071938dd
Замир Мусаев <zamir.ru@gmail.com>
2018-07-23 22:02:57 +0300

    Just strong coupled logging. Need wrappers

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 )
 
+type Adder interface {
+	Add(x, y int) int
+}
+
+type AdderFunc func(x, y int) int
+
+func (a AdderFunc) Add(x, y int) int {
+	return a(x, y)
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	a := AdderFunc(
+		func(x, y int) (result int) {
+			defer func(t time.Time) {
+				log.SetOutput(os.Stdout)
+				log.Printf("took=%v, x=%v, y=%v, result=%v", time.Since(t), x, y, result)
+			}(time.Now())
+			return x + y
+		},
+	)
+	fmt.Println(Do(a))
+}
+
+func Do(adder Adder) int {
+	return adder.Add(1, 2)
 }
